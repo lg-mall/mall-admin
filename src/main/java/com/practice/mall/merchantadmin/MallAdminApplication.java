657cfb9a34396a3185ee5e0905dfbb303a8e40c0
LeonGeroge <leongeroge@gmail.com>
2019-10-13 20:05:41 +0800

    rename package

--- src/main/java/com/practice/mall/merchantadmin/MallAdminApplication.java
+++ src/main/java/com/practice/mall/merchantadmin/MallAdminApplication.java
@@ -1,4 +1,4 @@
-package com.practice.mall.admin;
+package com.practice.mall.merchantadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
